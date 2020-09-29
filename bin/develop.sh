3bfce2838acf5178539225a890430eaaa4bf750d
KJ Price <kjprice12@gmail.com>
2020-09-29 08:04:53 -0500

    Create servo module

--- bin/develop.sh
+++ bin/develop.sh
@@ -5,6 +5,7 @@ cd ../python
 source ~/.bash_profile
 ca
 
-script_to_run=process-image-for-servo.py
+script_to_run=servo-module.py
+# script_to_run=process-image-for-servo.py
 
 nodemon -e py,sh -x "time python $script_to_run"
---
+++ python/servo-module.py
@@ -0,0 +1,95 @@
+import time
+import unittest
+
+IS_TEST = False
+if __name__ == '__main__':
+    IS_TEST = True
+
+if not IS_TEST:
+    import RPi.GPIO as GPIO
+
+# Settings
+SERVO_PIN = 7 # Can be any IO pins including: 7,11,12,13,15,16,18,22
+
+
+def setup_servo_main():
+
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(SERVO_PIN, GPIO.OUT)
+
+    servo = GPIO.PWM(SERVO_PIN, 50) # 50 Hz pulse
+
+    return servo
+
+class StubServo():
+    def start(self, position):
+        None
+    def ChangeDutyCycle(self, n):
+        None
+
+class Servo():
+    # Duty can go from 2-12% (0-180 degrees)
+    duty_range = [2, 12]
+    current_position = 0
+    possible_positions = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
+    def __init__(self):
+        if IS_TEST:
+            self.servo = StubServo()
+        else:
+            self.servo = setup_servo_main()
+        
+        self.servo.start(0)
+
+    def go_to(self, position):
+        if position not in self.possible_positions:
+            return
+
+        self.current_position = position
+
+        duty = position + 7
+        self.servo.ChangeDutyCycle(duty)
+    def reset(self):
+        self.go_to(0)
+    def move_left(self):
+        self.go_to(self.current_position - 1)
+    def move_right(self):
+        self.go_to(self.current_position + 1)
+    def teardown(self):
+        print('shutting down servo')
+        if not IS_TEST:
+            GPIO.cleanup()
+
+class TestServoModule(unittest.TestCase):
+    def setUp(self):
+        self.servo = Servo()
+    def test_1_start_servo(self):
+        self.assertEqual(self.servo.current_position, 0)
+    def test_2_servo_positions(self):
+        first_position = self.servo.possible_positions[0]
+        last_position = self.servo.possible_positions[-1]
+
+        self.assertEqual(first_position + 7, self.servo.duty_range[0])
+        self.assertEqual(last_position + 7, self.servo.duty_range[-1])
+
+    def test_3_move_servo(self):
+        self.assertEqual(self.servo.current_position, 0)
+
+        self.servo.move_left()
+        self.assertEqual(self.servo.current_position, -1)
+        self.servo.reset()
+        self.assertEqual(self.servo.current_position, 0)
+        self.servo.move_right()
+        self.assertEqual(self.servo.current_position, 1)
+
+        self.servo.go_to(5)
+        self.assertEqual(self.servo.current_position, 5)
+        self.servo.move_right()
+        self.assertEqual(self.servo.current_position, 5)
+        
+        self.servo.go_to(-5)
+        self.assertEqual(self.servo.current_position, -5)
+        self.servo.move_left()
+        self.assertEqual(self.servo.current_position, -5)
+
+if IS_TEST:
+    unittest.main()
