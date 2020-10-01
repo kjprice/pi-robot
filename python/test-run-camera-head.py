a854001f3b59c1538a83aa72dfa35d6a0206b75a
KJ Price <kjprice12@gmail.com>
2020-10-01 09:03:20 -0500

    Test code for turning servo slowly

--- python/test-run-camera-head.py
+++ python/test-run-camera-head.py
@@ -1,21 +1,34 @@
 #!/usr/bin/python3
-# Taken from https://www.youtube.com/watch?v=xHDT4CwjUQE
+
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
 import RPi.GPIO as GPIO
+import sys
 import time
 
 # Settings
 SERVO_PIN = 7 # Can be any IO pins including: 7,11,12,13,15,16,18,22
-TIME_WAIT = 2 # seconds
 
 
 servo = None
-
-print('Waiting for {} seconds'.format(TIME_WAIT))
-time.sleep(1)
+running_slowly = False
+current_duty = 7 # Right in the middle
 
 # Duty can go from 2-12% (0-180 degrees)
 DUTY_MIN = 2.2
 DUTY_MAX = 11.8
+
+def turn_servo(duty):
+    global current_duty
+    if duty < 2:
+        print('Number {} is too low. Ignoring'.format(n))
+        return
+    if duty > 12:
+        print('Number {} is too high. Ignoring'.format(n))
+        return
+    
+    current_duty = duty
+    servo.ChangeDutyCycle(duty)
 
 def turn_on():
     global servo
@@ -30,20 +43,44 @@ def turn_off():
     print('Shutting down')
     GPIO.cleanup()
 
-def wait():
-    time.sleep(1)
+def wait(seconds=1):
+    time.sleep(seconds)
 
 def run_low():
     print('Setting servo to beginning')
-    servo.ChangeDutyCycle(DUTY_MIN)
+    turn_servo(DUTY_MIN)
 
 def run_high():
     print('Setting servo to end')
-    servo.ChangeDutyCycle(DUTY_MAX)
+    turn_servo(DUTY_MAX)
 
 def run_middle():
     print('Turning back 90 degrees for 2 seconds')
-    servo.ChangeDutyCycle(7)
+    turn_servo(7)
+
+def start_running_slowly():
+    global running_slowly
+    print('starting to run slowly')
+
+    running_slowly = True
+
+def stop_running_slowly():
+    global running_slowly
+    print('ending process to run slowly')
+
+    running_slowly = False
+
+should_stop_immediately = False
+async def try_to_run_slowly(future):
+    global current_duty, running_slowly, should_stop_immediately
+    while True:
+        if should_stop_immediately:
+            future.set_result('Exiting "Running Slowly" command')
+            break
+        if running_slowly:
+            turn_servo(current_duty + 0.02)
+        await asyncio.sleep(0.02)
+    
 
 def set_specific_duty(n):
     try:
@@ -51,16 +88,9 @@ def set_specific_duty(n):
     except ValueError:
         return
 
-    if num < 2:
-        print('Number {} is too low. Ignoring'.format(n))
-        return
-    if num > 12:
-        print('Number {} is too high. Ignoring'.format(n))
-        return
-    
     print('Turning to {}'.format(num))
 
-    servo.ChangeDutyCycle(num)
+    turn_servo(num)
 
 def run_all_tests():
     run_low()
@@ -70,48 +100,79 @@ def run_all_tests():
     run_middle()
     wait()
 
-# TODO: Allow to switch from position 0-10 (mapped to 2-12) via the keyboard
-# TODO: Test what happens when you disconnect/reconnect the servo
-
-
 instructions = '''
 Please type any of the following commands:
-Positions: l=low | h=high | m=middle
-Positions(any): any number greater than 2
-State: on | off | e=exit
-Instructions: i = show-instructions
+Servo Instructions:
+    Positions: l=low | h=high | m=middle
+    Positions(any): any number greater than 2
+    Routine: s=take pictures slowly
+    State: on | off | e=exit
+    Instructions: i = show-instructions
+Camera Instructions:
+    Take Pictures: p5="5 pictures" | p10="10 pictures"
+
 '''
 
 def show_instructions():
     print(instructions)
 
 turn_on()
+turn_servo(current_duty)
 wait()
 show_instructions()
 
-while True:
-    x = input()
-    if x == '':
-        continue
+async def ainput(prompt: str = ""):
+    # TODO: remove the "print" stuff
+    with ThreadPoolExecutor(1, "AsyncInput", lambda x: print(x, end="", flush=True), (prompt,)) as executor:
+        return (await asyncio.get_event_loop().run_in_executor(
+            executor, sys.stdin.readline
+        )).rstrip()
 
-    if x == 'l':
-        run_low()
-    if x == 'm':
-        run_middle()
-    if x == 'h':
-        run_high()
-    if x == 'on':
-        turn_on()
-    if x == 'off':
-        turn_off()
-    if x == 'i':
-        show_instructions()
-    if x == 'e':
-        break
+async def handle_user_input(future):
+    global should_stop_immediately
+    while True:
+        x = await ainput('')
 
-    set_specific_duty(x)
-    x = ''
-        
+        if x == '':
+            continue
+
+        if x == 'l':
+            run_low()
+        if x == 'm':
+            run_middle()
+        if x == 'h':
+            run_high()
+        if x == 'on':
+            turn_on()
+        if x == 'off':
+            turn_off()
+        if x == 'i':
+            show_instructions()
+        if x == 'e':
+            should_stop_immediately = True
+            future.set_result('closing user input')
+            break
+        if x == 's':
+            start_running_slowly()
+        else: # If any other key was typed we want to stop running slowly
+            stop_running_slowly()
+
+        # This is a catch all to see if the user provided a specific number
+        set_specific_duty(x)
+        x = ''
+
+loop = asyncio.get_event_loop()
+
+user_input_future = asyncio.Future()
+asyncio.ensure_future(handle_user_input(user_input_future))
+
+run_slowly_future = asyncio.Future()
+asyncio.ensure_future(try_to_run_slowly(run_slowly_future))
+
+loop.run_until_complete(asyncio.gather(
+    user_input_future,
+    run_slowly_future
+))
 
 turn_off()
 
