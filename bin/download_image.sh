af072e99fb3c985930f78cdad7f78cf6fc31f328
KJ Price <kjprice12@gmail.com>
2020-10-03 20:01:01 -0500

    Download image

--- bin/download_image.sh
+++ bin/download_image.sh
@@ -1,1 +1,2 @@
-scp pirobot:~/Desktop/pi-camera.jpg data/downloaded-images/; open data/downloaded-images/pi-camera.jpg
+scp pirobot:~/Desktop/pi-camera.jpg data/downloaded-images/
+open data/downloaded-images/pi-camera.jpg
