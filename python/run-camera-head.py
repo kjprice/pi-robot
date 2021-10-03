8c71eca72a8e6bfd1dfd1a5e04c11aa2aa88c76a
KJ Price <kjprice12@gmail.com>
2021-10-02 21:19:18 -0500

    Throttle the number of images that are processed

--- python/run-camera-head.py
+++ python/run-camera-head.py
@@ -21,11 +21,14 @@ from modules.image_module import save_image
 from modules.image_processor import Image_Processor
 from modules.server_module import handle_default_server_response
 
+# We do not need too many images - it is ok to throw away some
+# TODO: Decide which images to throw away based on if they are more blurry than others
+MAX_IMAGES_TO_PROCESS_PER_SECOND = 20
+
 IS_TEST = False
 if 'IS_TEST' in os.environ:
     IS_TEST = True
 
-is_processing_server_online = False
 servo_url = get_servo_url(IS_TEST)
 
 def get_servo_url_path(path):
@@ -82,24 +85,55 @@ if not IS_TEST:
     test_connection_with_servo_server()
 time.sleep(1)
 
-def main():
-    global is_processing_server_online
-    is_processing_server_online = check_if_processing_server_is_online()
+class CameraHead():
+    is_processing_server_online = None
+    image_processor = None
+    minimum_time_in_seconds_between_images = None
+    count_images_discarded = None
+    time_started = None
+    last_image_received_time = None
 
-    image_processor = Image_Processor()
-    images_count = 0
-    for img, time_passed_for_image in image_generator(IS_TEST):
-        images_count += 1
-        # TODO: Periodically check to make sure that server is still online (every 10 seconds)
-        if is_processing_server_online:
-            # TODO: Decide whether to make greyscale before saving - compare time savings
-            print('Found {} image(s)'.format(images_count), end='\r')
-            save_image(img, 'image-raw.jpg')
-        else:
-            print('Processing server is offline - processing internally')
-            image_processor.process_message_immediately(img, time_passed_for_image)
+    def __init__(self) -> None:
+        self.image_processor = Image_Processor()
+        self.minimum_time_in_seconds_between_images = 1 / MAX_IMAGES_TO_PROCESS_PER_SECOND
+        self.is_processing_server_online = False
+        self.count_images_discarded = 0
+        self.time_started = time.time()
+
+    # TODO: This method should consider the quality of the image over other images - ie, drop if very blurry
+    def should_keep_image(self):
+        now = time.time()
+        if self.last_image_received_time is None:
+            self.last_image_received_time = now
+            return True
+        
+        if now - self.last_image_received_time < self.minimum_time_in_seconds_between_images:
+            return False
+        
+        self.last_image_received_time = now
+        return True
+
+    def run(self):
+        self.is_processing_server_online = check_if_processing_server_is_online()
+
+        images_count = 0
+        for img, time_passed_for_image in image_generator(IS_TEST):
+            if not self.should_keep_image():
+                self.count_images_discarded += 1
+                continue
+
+            images_count += 1
+            # TODO: Periodically check to make sure that server is still online (every 10 seconds)
+            if self.is_processing_server_online:
+                # TODO: Decide whether to make greyscale before saving - compare time savings
+                print('Found {} image(s) and dropped {} image(s)'.format(images_count, self.count_images_discarded), end='\r')
+                save_image(img, 'image-raw.jpg')
+            else:
+                self.image_processor.process_message_immediately(img, time_passed_for_image)
+
 
 if __name__ == "__main__":
-    main()
+    camera_head = CameraHead()
+    camera_head.run()
 
 atexit.register(shutdown_camera)
