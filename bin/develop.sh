381b954e50607d413b3e68f35e0b4ea061936d29
KJ Price <kjprice12@gmail.com>
2021-10-21 15:11:56 -0500

    Rename files in shell script

--- bin/develop.sh
+++ bin/develop.sh
@@ -5,10 +5,10 @@ cd ../python
 source ~/.bash_profile
 ca
 
-script_to_run=run-camera-head.py
+# script_to_run=run_camera_head.py
 # script_to_run=modules/process_image_for_servo.py
-# script_to_run=modules/servo_module.py
-# script_to_run=run-servo-server.py
+script_to_run=modules/servo_module.py
+# script_to_run=run_servo_server.py
 
 
 nodemon -e py,sh -x "time IS_TEST=true python3.6 $script_to_run"
