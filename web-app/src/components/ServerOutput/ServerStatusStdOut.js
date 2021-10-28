1b70277338cb146743ab487bae1140ba7299789e
KJ Price <kjprice12@gmail.com>
2021-10-27 19:20:32 -0500

    Refactor ServerStatusStdOut

--- web-app/src/components/ServerOutput/ServerStatusStdOut.js
+++ web-app/src/components/ServerOutput/ServerStatusStdOut.js
@@ -14,36 +14,50 @@ function getServerContainerColumnClass(serverNamesCount) {
   return `col-${colSize}`;
 }
 
+function Message(props) {
+  const { statusMessage } = props;
+  const { timestamp, message } = statusMessage;
+  return (
+    <code key={timestamp}>
+      <div className="row">
+        <div className="col">
+          {timestamp}
+        </div>
+        <div className="col">
+          {message}
+        </div>
+      </div>
+    </code>      
+  );
+}
+
+function ServerOutputContainer(props) {
+  const { serverName, containerClass, statuses, } = props;
+  return (
+    <div key={serverName} className={containerClass}>
+      {statuses.map(statusMessage => {
+        return <Message statusMessage={statusMessage} />
+      })}
+    </div>
+  )
+}
+
 function ServerStatusStdOut({serverOutputByProcessName}) {
   const serverNames = Object.keys(serverOutputByProcessName);
   const serverNamesCount = serverNames.length;
 
   const containerClass = getServerContainerColumnClass(serverNamesCount);
 
-  // TODO: Refactor
   return (
     <div className="row server-output">
       {serverNames.map(serverName => {
         const statuses = serverOutputByProcessName[serverName];
-        return (
-          <div key={serverName} className={containerClass}>
-            {statuses.map(stuatusMessage => {
-              const { timestamp, message } = stuatusMessage;
-              return (
-                <code key={timestamp}>
-                  <div className="row">
-                    <div className="col">
-                      {timestamp}
-                    </div>
-                    <div className="col">
-                      {message}
-                    </div>
-                  </div>
-                </code>      
-              );
-            })}
-          </div>
-        )
+
+        return <ServerOutputContainer
+          serverName={serverName}
+          containerClass={containerClass}
+          statuses={statuses}
+        />
       })}
     </div>
   );
