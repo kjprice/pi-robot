d9ea70a6587e65250a5f473a593e8c65f0a2399f
KJ Price <kjprice12@gmail.com>
2022-06-15 11:24:15 -0500

    Add full alphabet

--- python/modules/morse_code/morse_letters.py
+++ python/modules/morse_code/morse_letters.py
@@ -1,4 +1,5 @@
 from typing import List
+import unittest
 
 from .morse_code_units import MorseCodeUnits
 
@@ -17,8 +18,31 @@ def add_spaces(units: List[MorseCodeUnits]) -> List[MorseCodeUnits]:
 
 MORSE_LETTERS = {
     'A': add_spaces([DOT, DASH]),
+    'B': add_spaces([DASH, DOT, DOT, DOT]),
+    'C': add_spaces([DASH, DOT, DASH, DOT]),
+    'D': add_spaces([DASH, DOT, DOT]),
     'E': [DOT],
+    'F': add_spaces([DOT, DOT, DASH, DOT]),
+    'G': add_spaces([DASH, DASH, DOT]),
+    'H': add_spaces([DOT, DOT, DOT, DOT]),
+    'I': add_spaces([DOT, DOT]),
+    'J': add_spaces([DOT, DASH, DASH, DASH]),
+    'K': add_spaces([DASH, DOT, DASH]),
+    'L': add_spaces([DOT, DASH, DOT, DOT]),
+    'M': add_spaces([DASH, DASH]),
+    'N': add_spaces([DASH, DOT]),
+    'O': add_spaces([DASH, DASH, DASH]),
+    'P': add_spaces([DOT, DASH, DASH, DOT]),
+    'Q': add_spaces([DASH, DASH, DOT, DASH]),
+    'R': add_spaces([DOT, DASH, DOT]),
+    'S': add_spaces([DOT, DOT, DOT]),
     'T': [DASH],
+    'U': add_spaces([DOT, DOT, DASH]),
+    'V': add_spaces([DOT, DOT, DOT, DASH]),
+    'W': add_spaces([DOT, DASH, DASH]),
+    'X': add_spaces([DASH, DOT, DOT, DASH]),
+    'Y': add_spaces([DASH, DOT, DASH, DASH]),
+    'Z': add_spaces([DASH, DASH, DOT, DOT]),
 }
 
 # Turns List[MorseCodeUnits] into a string of their values
@@ -41,3 +65,24 @@ def morse_units_to_letter(morse_units):
         return MORSE_UNITS_TO_LETTER[units_str]
     
     return '?'
+
+class TestMorseLetters(unittest.TestCase):
+    def test_add_spaces(self):
+        input = [DOT, DASH]
+        expected_output = [DOT, SPACE, DASH]
+
+        output = add_spaces(input)
+
+        self.assertEqual(output, expected_output)
+        self.assertEqual(output, MORSE_LETTERS['A'])
+
+    def test_morse_letters_dict(self):
+        self.assertEqual(len(MORSE_LETTERS), 26)
+
+    def test_morse_units_to_letters_dict(self):
+        self.assertEqual(len(MORSE_UNITS_TO_LETTER), 26)
+
+        letters_from_units = list(MORSE_UNITS_TO_LETTER.values())
+        letters_keys = list(MORSE_LETTERS.keys())
+        
+        self.assertEqual(letters_from_units, letters_keys)
--- python/tests.py
+++ python/tests.py
@@ -1,10 +1,12 @@
 import unittest
 from .modules.morse_code.morse_code import TestMorseCode
 from .modules.morse_code.morse_state_size import TestMorseCodeStateSize
+from .modules.morse_code.morse_letters import TestMorseLetters
 
 suite = unittest.TestSuite()
 suite.addTest(unittest.makeSuite(TestMorseCode))
 suite.addTest(unittest.makeSuite(TestMorseCodeStateSize))
+suite.addTest(unittest.makeSuite(TestMorseLetters))
 
 runner = unittest.TextTestRunner()
 runner.run(suite)
