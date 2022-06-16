a67bb1888dcf2d4a473b907379e863bc469492a7
KJ Price <kjprice12@gmail.com>
2022-06-16 16:54:09 -0500

    Raspberry pi blink 5 times

--- bin/freenove_starter_kit/run_freenove.sh
+++ bin/freenove_starter_kit/run_freenove.sh
@@ -5,4 +5,5 @@ cd ../..
 # This gets run locally
 scp -r python/freenove pi3misc2:~/Projects/pirobot/python/
 
+ssh pi3misc2 'sh ~/Projects/pirobot/bin/kill_process_by_name.sh freenove &> /dev/null'
 ssh pi3misc2 'python ~/Projects/pirobot/python/freenove/main.py'
--- python/freenove/main.py
+++ python/freenove/main.py
@@ -1,4 +1,1 @@
 import ch1_blink
-
-print('boom')
-print(ch1_blink)
