4314274c9e72de45ac47883f9b16540d578107ac
KJ Price <kjprice12@gmail.com>
2022-06-17 16:21:50 -0500

    Decouple logic for running for specified duration

--- python/freenove/ch02_button_led.py
+++ python/freenove/ch02_button_led.py
@@ -2,6 +2,8 @@
 import RPi.GPIO as GPIO
 import time
 
+from typing import Callable
+
 LED_PIN = 11
 BUTTON_PIN = 12
 
@@ -10,23 +12,33 @@ def setup():
     GPIO.setup(LED_PIN, GPIO.OUT)
     GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-def run_during_seconds(seconds: int):
+def alert_ready():
+    GPIO.output(LED_PIN, GPIO.HIGH)
+    time.sleep(1)
+
+def light_when_pressed():
+    if GPIO.input(BUTTON_PIN) == GPIO.LOW:
+        GPIO.output(LED_PIN, GPIO.HIGH)
+    else:
+        GPIO.output(LED_PIN, GPIO.LOW)
+
+def run_for_duration(callback: Callable, seconds: int):
     t_start = time.time()
     while True:
         t_end = time.time()
         if (t_end - t_start) > seconds:
             break
 
-        if GPIO.input(BUTTON_PIN) == GPIO.LOW:
-            GPIO.output(LED_PIN, GPIO.HIGH)
-        else:
-            GPIO.output(LED_PIN, GPIO.LOW)
+        callback()
+        
 
 def close():
     GPIO.cleanup()
 
 setup()
 
-run_during_seconds(5)
+alert_ready()
+
+run_for_duration(light_when_pressed, 5)
 
 close()
