ed27861181c64390730ca0732590e695bf24b565
KJ Price <kjprice12@gmail.com>
2022-06-17 16:36:32 -0500

    Ch 2.2 Table Lamp

--- python/freenove/ch02_button_led.py
+++ python/freenove/ch02_button_led.py
@@ -16,20 +16,47 @@ def alert_ready():
     GPIO.output(LED_PIN, GPIO.HIGH)
     time.sleep(1)
 
+# Ch 2.1 
 def light_when_pressed():
     if GPIO.input(BUTTON_PIN) == GPIO.LOW:
         GPIO.output(LED_PIN, GPIO.HIGH)
     else:
         GPIO.output(LED_PIN, GPIO.LOW)
 
-def run_for_duration(callback: Callable, seconds: int):
+# Ch 2.2
+# Keep light on or off until button is pressed
+def table_lamp():
+    class TableLamp():
+        button_state = GPIO.LOW
+        def __init__(self) -> None:
+            self.button_state = GPIO.LOW
+            self.set_led()
+            GPIO.add_event_detect(BUTTON_PIN, GPIO.FALLING, callback=self.button_press, bouncetime=300)
+        def button_press(self, channel):
+            if channel != BUTTON_PIN:
+                raise Exception('Expected pin on channel {} to be pressed'.format(channel))
+            
+            self.button_state = GPIO.HIGH if self.button_state == GPIO.LOW else GPIO.LOW
+            print(channel, LED_PIN, self.button_state)
+            self.set_led()
+            
+        def set_led(self):
+            GPIO.output(LED_PIN, self.button_state)
+
+    TableLamp()
+
+def run_for_duration(seconds: int, call_once: Callable = None, call_continuously: Callable = None ):
     t_start = time.time()
+    if call_once:
+        call_once()
+
     while True:
         t_end = time.time()
         if (t_end - t_start) > seconds:
             break
 
-        callback()
+        if call_continuously:
+            call_continuously()
         
 
 def close():
@@ -39,6 +66,7 @@ setup()
 
 alert_ready()
 
-run_for_duration(light_when_pressed, 5)
+# run_for_duration(5, call_continuously=light_when_pressed)
+run_for_duration(5, call_once=table_lamp)
 
 close()
