5a81541aa1a23677894b22e71f189272bb815f4a
KJ Price <kjprice12@gmail.com>
2022-06-24 18:27:22 -0500

    rsync (not scp)

--- bin/download/download_security_videos.sh
+++ bin/download/download_security_videos.sh
@@ -3,6 +3,4 @@ cd "$(dirname "$0")"
 DATA_DIR=data/security_videos
 cd ../..
 
-# mkdir -p $DATA_DIR
-
-scp -r pi@pi3misc2:~/Projects/pirobot/$DATA_DIR/ data/
+rsync -r pi@pi3misc2:~/Projects/pirobot/$DATA_DIR data/
