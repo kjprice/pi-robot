3c5ce5b2b14447271d061674e2f8d8aed402469a
KJ Price <kjprice12@gmail.com>
2022-06-25 13:46:24 -0500

    Actually log output (using tee)

--- bin/run/run_security_camera.sh
+++ bin/run/run_security_camera.sh
@@ -5,11 +5,11 @@ cd "$(dirname "$0")"
 cd ../..
 
 # How many seconds each video should capture
-SECONDS=10
+SECONDS=1
 MS=$(expr $SECONDS*1000 | bc)
 
 DATA_DIR=data/security_videos
-LOGS_DIR=data/logs
+LOGS_DIR=data/logs/security_videos
 mkdir -p $DATA_DIR
 mkdir -p $LOGS_DIR
 
@@ -18,8 +18,9 @@ raw_filepath=$DATA_DIR/$filename.h264
 mp4_filepath=$DATA_DIR/$filename.mp4
 
 ALL_LOGS_FILEPATH=$LOGS_DIR/all.txt
+
 THIS_LOG_FILEPATH=$LOGS_DIR/video_$filename.txt
 
 # Take video transformed to mp4
-time libcamera-vid -t $MS -o $raw_filepath 2&> $THIS_LOG_FILEPATH 2&> $ALL_LOGS_FILEPATH
-time MP4Box -add $raw_filepath $mp4_filepath 2&> $THIS_LOG_FILEPATH 2&> $ALL_LOGS_FILEPATH
+time libcamera-vid -t $MS -o $raw_filepath |& tee -a $ALL_LOGS_FILEPATH $THIS_LOG_FILEPATH
+time MP4Box -add $raw_filepath $mp4_filepath |& tee -a $ALL_LOGS_FILEPATH $THIS_LOG_FILEPATH
