7db31f4889c65d7b1a7e4655948bc70b0a492dbb
KJ Price <kjprice12@gmail.com>
2022-06-25 13:05:31 -0500

    Attempt at writing logs

--- bin/run/run_security_camera.sh
+++ bin/run/run_security_camera.sh
@@ -17,6 +17,9 @@ filename=$(date '+%Y-%m-%d_%H_%M_%S')
 raw_filepath=$DATA_DIR/$filename.h264
 mp4_filepath=$DATA_DIR/$filename.mp4
 
+ALL_LOGS_FILEPATH=$LOGS_DIR/all.txt
+THIS_LOG_FILEPATH=$LOGS_DIR/video_$filename.txt
+
 # Take video transformed to mp4
-time libcamera-vid -t $MS -o $raw_filepath
-time MP4Box -add $raw_filepath $mp4_filepath
+time libcamera-vid -t $MS -o $raw_filepath 2&> $THIS_LOG_FILEPATH 2&> $ALL_LOGS_FILEPATH
+time MP4Box -add $raw_filepath $mp4_filepath 2&> $THIS_LOG_FILEPATH 2&> $ALL_LOGS_FILEPATH
