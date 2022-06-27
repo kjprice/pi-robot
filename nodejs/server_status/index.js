dd9362cd90d0e24b7c3f6c7663602f34fed60f35
KJ Price <kjprice12@gmail.com>
2022-06-27 09:02:01 -0500

    Socket.io WIP

--- nodejs/server_status/index.js
+++ nodejs/server_status/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 const port = require('../../config.json')['healthStatusPort']
 
 app.get('/ping', (req, res) => {
   res.send('success');
 })
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
