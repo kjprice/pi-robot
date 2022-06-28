ff93b81f7df6d44a6a85708f25162eab0edcf273
KJ Price <kjprice12@gmail.com>
2022-06-27 21:56:43 -0500

    Remove unused import

--- web-app/src/components/RaspberryPi/index.js
+++ web-app/src/components/RaspberryPi/index.js
@@ -1,6 +1,5 @@
 import { useParams } from "react-router-dom";
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 
 const mapStateToProps = (props) => {
   const { serverReducers } = props;
