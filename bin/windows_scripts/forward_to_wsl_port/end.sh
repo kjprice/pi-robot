e4ef3c2f3742c5c711fed89142a9eea7766a8f90
KJ Price <kjprice12@gmail.com>
2022-06-30 17:23:52 -0500

    Also forward image hub port (6666 currently)

--- bin/windows_scripts/forward_to_wsl_port/end.sh
+++ bin/windows_scripts/forward_to_wsl_port/end.sh
@@ -2,13 +2,20 @@
 cd "$(dirname "$0")"
 
 WSL_IP=`wsl hostname -I`
-PORT=`../misc/get_config.sh ports.webServerPort`
+WEB_SERVER_PORT=`../misc/get_config.sh ports.webServerPort`
+IMAGE_HUB_PORT=`../misc/get_config.sh ports.imageHubPort`
 
 WINDOWS_IP=`ipconfig|grep -m 1 IPv4|sed "s/IPv4 Address. . . . . . . . . . . : //g" | xargs`
 
-cmd="netsh interface portproxy delete v4tov4 listenport=$PORT listenaddress=$WINDOWS_IP"
-echo "Canceling IP Forward to WSL using the command:"
-echo $cmd
-echo
-echo `$cmd`
+function kill_on_port() {
+    PORT=$1
+    cmd="netsh interface portproxy delete v4tov4 listenport=$PORT listenaddress=$WINDOWS_IP"
+    echo "Canceling IP Forward to WSL using the command:"
+    echo $cmd
+    echo
+    echo `$cmd`
+    echo
+}
 
+kill_on_port $WEB_SERVER_PORT
+kill_on_port $IMAGE_HUB_PORT
--- bin/windows_scripts/forward_to_wsl_port/start.sh
+++ bin/windows_scripts/forward_to_wsl_port/start.sh
@@ -2,12 +2,20 @@
 cd "$(dirname "$0")"
 
 WSL_IP=`wsl hostname -I`
-PORT=`../misc/get_config.sh ports.webServerPort`
+WEB_SERVER_PORT=`../misc/get_config.sh ports.webServerPort`
+IMAGE_HUB_PORT=`../misc/get_config.sh ports.imageHubPort`
 
 WINDOWS_IP=`ipconfig|grep -m 1 IPv4|sed "s/IPv4 Address. . . . . . . . . . . : //g" | xargs`
 
-cmd="netsh interface portproxy add v4tov4 listenport=$PORT listenaddress=$WINDOWS_IP connectport=$PORT connectaddress=$WSL_IP"
-echo "Forwarding IP to WSL using the command:"
-echo $cmd
-echo
-echo `$cmd`
+function run_on_port() {
+    PORT=$1
+    cmd="netsh interface portproxy add v4tov4 listenport=$PORT listenaddress=$WINDOWS_IP connectport=$PORT connectaddress=$WSL_IP"
+    echo "Forwarding IP to WSL using the command:"
+    echo $cmd
+    echo
+    echo `$cmd`
+    echo 
+}
+
+run_on_port $WEB_SERVER_PORT
+run_on_port $IMAGE_HUB_PORT
