a8b2009397db5ed4a632a4417e90350760396ced
KJ Price <kjprice12@gmail.com>
2022-06-30 10:22:18 -0500

    Create script to forward HTTP to WSL with no parameters

--- bin/windows_scripts/forward_to_wsl_port/end.sh
+++ bin/windows_scripts/forward_to_wsl_port/end.sh
@@ -6,8 +6,9 @@ PORT=9898 # TODO: Pull from config.json
 
 WINDOWS_IP=`ipconfig|grep -m 1 IPv4|sed "s/IPv4 Address. . . . . . . . . . . : //g" | xargs`
 
-cmd="netsh interface portproxy add v4tov4 listenport=$PORT listenaddress=$WINDOWS_IP connectport=$PORT connectaddress=$WSL_IP"
-echo "Forwarding IP to WSL using the command:"
+cmd="netsh interface portproxy delete v4tov4 listenport=$PORT listenaddress=$WINDOWS_IP"
+echo "Canceling IP Forward to WSL using the command:"
 echo $cmd
 echo
 echo `$cmd`
+
