c0cc1efbc9d2d26b46945aea0a316a149bdfd48f
KJ Price <kjprice12@gmail.com>
2022-06-30 20:29:52 -0500

    Display health status links for each raspberry pi

--- web-app/src/components/RaspberryPi/index.js
+++ web-app/src/components/RaspberryPi/index.js
@@ -6,11 +6,12 @@ const mapStateToProps = (props) => {
   const { config } = serverReducers;
   const { ports } = config;
 
-  const { webminPort, pythonFileSystemServerPort } = ports || {};
+  const { webminPort, pythonFileSystemServerPort, healthStatusPort } = ports || {};
 
   return {
     webminPort,
     pythonFileSystemServerPort,
+    healthStatusPort,
   };
 };
 
@@ -26,19 +27,42 @@ const DataDirectoryLink = ({hostname, port}) => {
   return <a href={link} rel="noreferrer" target="_blank">Data Directory</a>;
 }
 
+const ServerStatusLink = ({hostname, port, endpoint}) => {
+  const link = `http://${hostname}:${port}/${endpoint}`;
+
+  return <a href={link} rel="noreferrer" target="_blank">{endpoint}</a>;
+}
+
+const ServerStatusPingLink = (props) => {
+  return <ServerStatusLink {...props} endpoint="ping" />
+}
+
+const ServerStatusProcessesLink = (props) => {
+  return <ServerStatusLink {...props} endpoint="processes" />
+}
+
 function RaspberryPi(props) {
-  const { webminPort, pythonFileSystemServerPort } = props;
+  const { webminPort, pythonFileSystemServerPort, healthStatusPort } = props;
   let params = useParams();
   const { hostname } = params;
 
   console.log({props, params, hostname});
   return (
     <div>
-      <h3>{hostname}</h3>
+      <h2>{hostname}</h2>
+
+      <h4>Internal Links</h4>
       <ul className="list-unstyled">
         <li><WebminLink hostname={hostname} port={webminPort} /></li>
         <li><DataDirectoryLink hostname={hostname} port={pythonFileSystemServerPort} /></li>
       </ul>
+
+      <h4>Server Status Links</h4>
+      <ul className="list-unstyled">
+        <li><ServerStatusPingLink hostname={hostname} port={healthStatusPort} /></li>
+        <li><ServerStatusProcessesLink hostname={hostname} port={healthStatusPort} /></li>
+      </ul>
+      
     </div>
 )
 }
