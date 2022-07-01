873d588dc6a3bb36030c69f24956830397f47972
KJ Price <kjprice12@gmail.com>
2022-06-30 20:18:38 -0500

    Add endpoint to server_status to get active processes

--- nodejs/server_status/index.js
+++ nodejs/server_status/index.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-const port = require('../../config.json')['ports']['healthStatusPort']
+const port = require('../../config.json')['ports']['healthStatusPort'];
 
 const log = (str) => {
   const date = new Date();
@@ -14,11 +14,21 @@ const log = (str) => {
 
 app.get('/ping', (req, res) => {
   const ip = req.socket.remoteAddress || req.ip || req.headers['x-real-ip'];
-  const date = new Date();
+
   log(`hit "${req.method} ${req.url}" from ip: "${ip}"`);
 
   res.status(200).send('success');
-})
+});
+
+app.get('/processes', (req, res) => {
+  const ip = req.socket.remoteAddress || req.ip || req.headers['x-real-ip'];
+  log(`hit "${req.method} ${req.url}" from ip: "${ip}"`);
+
+  const processes = require('../../data/processes.json');
+  const processNames = Object.keys(processes);
+
+  res.status(200).send(processNames);
+});
 
 io.on('connection', (socket) => {
   console.log('a user connected');
