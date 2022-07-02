ca75b5a5e61952af774ad992db4556996511014f
KJ Price <kjprice12@gmail.com>
2022-07-02 12:58:56 -0500

    Decouple RaspiServerInfo class

--- python/modules/raspi_info/raspi_poller.py
+++ python/modules/raspi_info/raspi_poller.py
@@ -1,88 +1,16 @@
 import time
-from typing import List
 
-from simplejson.errors import JSONDecodeError
-import requests
-
-from ..config import SERVER_NAMES, load_json_config
+from ..config import SERVER_NAMES
 from ..server_module.server_module import ServerModule
+from .raspi_server_info import RaspiServerInfo, setup_server_info
 
 DELAY_BETWEEN_REQUESTS = 5
-
-# TODO: add method for "toString" equivalent
-class PollServer:
-    is_online = None
-    hostname = None
-    address = None
-    processes = None
-    def __init__(self, hostname: str, port: int) -> None:
-        self.is_online = False
-        self.hostname = hostname
-        self.processes = []
-        self.address = f'http://{hostname}:{port}'
-    
-    def run_ping(self):
-        ping_url = f'{self.address}/ping'
-        is_server_online = False
-        SECONDS_BEFORE_TIMEOUT = 2
-        try:
-            r = requests.get(ping_url, timeout=SECONDS_BEFORE_TIMEOUT)
-            is_server_online = r.status_code == 200
-        except requests.exceptions.ConnectionError:
-            is_server_online = False
-        
-        return is_server_online
-
-    def fetch_active_processes(self):
-        url = f'{self.address}/processes'
-        r = requests.get(url)
-        try:
-            return r.json()
-        except JSONDecodeError:
-            return []
-    
-    def has_active_processes_changed(self):
-        processes = self.fetch_active_processes()
-
-        if self.processes == processes:
-            return False
-        
-        self.processes = processes
-        return True
-
-    def hasStatusChanged(self):
-        is_server_online = self.run_ping()
-
-        if self.is_online == is_server_online:
-            return False
-
-        self.is_online = is_server_online
-        return True
-    
-    def to_json(self):
-        return {
-            'hostname': self.hostname,
-            'is_online': self.is_online,
-            'processes': self.processes,
-        }
-
-def setup_servers() -> List[PollServer]:
-    servers = []
-    config = load_json_config()
-    server_hostnames = config['serverHostnames']
-    server_port = config['portsByProcess']['nodeServerStatus']
-    for server_hostname in server_hostnames:
-        poll_server = PollServer(server_hostname, server_port)
-        servers.append(poll_server)
-    
-    return servers
-
 
 # Continuously polls raspberry pis to get their status
 class RaspiPoller(ServerModule):
     poll_servers = None
     def __init__(self, arg_flags):
-        self.poll_servers = setup_servers()
+        self.poll_servers = setup_server_info()
 
         server_name = SERVER_NAMES.RASPI_POLLER
         super().__init__(server_name, arg_flags)
@@ -105,10 +33,10 @@ class RaspiPoller(ServerModule):
         servers = [server.to_json() for server in self.poll_servers]
         self.emit('all_raspi_statuses', servers)
 
-    def on_status_change(self, server: PollServer) -> None:
+    def on_status_change(self, server: RaspiServerInfo) -> None:
         self.emit('raspi_status_changed', server.to_json())
     
-    def on_active_process_change(self, server: PollServer) -> None:
+    def on_active_process_change(self, server: RaspiServerInfo) -> None:
         self.emit('raspi_active_processes_changed', server.to_json())
 
     def run_continuously(self):
---
+++ python/modules/raspi_info/raspi_server_info.py
@@ -0,0 +1,75 @@
+from typing import List
+
+from simplejson.errors import JSONDecodeError
+import requests
+
+from ..config import load_json_config
+
+
+# TODO: add method for "toString" equivalent
+class RaspiServerInfo:
+    is_online = None
+    hostname = None
+    address = None
+    processes = None
+    def __init__(self, hostname: str, port: int) -> None:
+        self.is_online = False
+        self.hostname = hostname
+        self.processes = []
+        self.address = f'http://{hostname}:{port}'
+    
+    def run_ping(self):
+        ping_url = f'{self.address}/ping'
+        is_server_online = False
+        SECONDS_BEFORE_TIMEOUT = 2
+        try:
+            r = requests.get(ping_url, timeout=SECONDS_BEFORE_TIMEOUT)
+            is_server_online = r.status_code == 200
+        except requests.exceptions.ConnectionError:
+            is_server_online = False
+        
+        return is_server_online
+
+    def fetch_active_processes(self):
+        url = f'{self.address}/processes'
+        r = requests.get(url)
+        try:
+            return r.json()
+        except JSONDecodeError:
+            return []
+    
+    def has_active_processes_changed(self):
+        processes = self.fetch_active_processes()
+
+        if self.processes == processes:
+            return False
+        
+        self.processes = processes
+        return True
+
+    def hasStatusChanged(self):
+        is_server_online = self.run_ping()
+
+        if self.is_online == is_server_online:
+            return False
+
+        self.is_online = is_server_online
+        return True
+    
+    def to_json(self):
+        return {
+            'hostname': self.hostname,
+            'is_online': self.is_online,
+            'processes': self.processes,
+        }
+
+def setup_server_info() -> List[RaspiServerInfo]:
+    servers = []
+    config = load_json_config()
+    server_hostnames = config['serverHostnames']
+    server_port = config['portsByProcess']['nodeServerStatus']
+    for server_hostname in server_hostnames:
+        poll_server = RaspiServerInfo(server_hostname, server_port)
+        servers.append(poll_server)
+    
+    return servers
