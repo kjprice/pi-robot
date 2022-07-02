0d1cace2c2f189015b8454cbc3ea88254c4a7a5f
KJ Price <kjprice12@gmail.com>
2022-07-01 23:33:07 -0500

    Loop through all processes on port to kill

--- bin/misc/kill_process_on_port.sh
+++ bin/misc/kill_process_on_port.sh
@@ -1,7 +1,7 @@
 port=$1
 process_id=$(lsof -i :$port -t)
 
-if [ ! -z $process_id ]; then
+for i in $(lsof -i :$port -t); do
   echo killing process $process_id on port $port
   kill $process_id
-fi
+done
