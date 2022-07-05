8ac3c99aa36c116bf96bf2fe9a65699d3a9a293e
KJ Price <kjprice12@gmail.com>
2022-07-05 09:24:33 -0500

    Kill robot camera head when running security camera

--- bin/start_process/securityCamera.sh
+++ bin/start_process/securityCamera.sh
@@ -6,6 +6,9 @@ log_path=$1
 
 source ../misc/setup_shell.sh
 
+# Kill services that might use the camera
+../misc/kill_process_by_name.sh robotCameraHead >> ../../$log_path 2>&1
+
 ../run/run_security_camera_forever.sh >> ../../$log_path 2>&1 &
 
 process_id="$!"
