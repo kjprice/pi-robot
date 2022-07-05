eeb8f2e121edaaf213b64ee5ca6b53bf8eb1b667
KJ Price <kjprice12@gmail.com>
2022-07-05 09:38:14 -0500

    Point front end to windows (tmp)

--- web-app/src/api/socket.js
+++ web-app/src/api/socket.js
@@ -2,8 +2,8 @@ import { io } from 'socket.io-client';
 
 export const SOCKET_ROOM_NAME = 'browsers'
 // TODO Move to config
-const SOCKET_URI = 'kj-macbook.lan:9898/';
-// const SOCKET_URI = 'desktop-u1e9rtd:9898/';
+// const SOCKET_URI = 'kj-macbook.lan:9898/';
+const SOCKET_URI = 'desktop-u1e9rtd:9898/';
 
 const socket = io(SOCKET_URI);
 
