28b9aabef123cbc7284ed92de2456279ec0120b9
KJ Price <kjprice12@gmail.com>
2022-07-06 17:05:54 -0500

    Add nvm bin to path when setting up shell scripts

--- bin/misc/setup_shell.sh
+++ bin/misc/setup_shell.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
-#### This is to help consolidate different python scripts so we always use the same version
+#### This is to help consolidate different node/python scripts so we always use the same version
 
 # Needed for some non-interactive scripts (ie. "Windows Ubuntu WSL")
 shopt -s expand_aliases
 
-if test -f "/Users/kjprice/anaconda3/envs/python3.6/bin/python3.6"; then
-  alias python=/Users/kjprice/anaconda3/envs/python3.6/bin/python3.6
-elif [ -f /usr/bin/python3 ]; then
-    alias python=/usr/bin/python3
-fi
+function get_first_word() {
+  cat - | head -1 | awk '{print $1}'
+}
+
+function get_nvm_bin_filepath() {
+  nvm_node_folder=~/.nvm/versions/node
+  if [ -d $nvm_node_folder ]; then
+    version=`ls "$nvm_node_folder" | get_first_word`
+    path=$nvm_node_folder/$version/bin
+    echo $path
+  fi
+}
+
+function setup_node() {
+  node_path=`get_nvm_bin_filepath`
+  if [ ! -z $node_path ]; then
+    PATH="$PATH:$node_path"
+  fi
+}
+
+function setup_python() {
+  if test -f "/Users/kjprice/anaconda3/envs/python3.6/bin/python3.6"; then
+    alias python=/Users/kjprice/anaconda3/envs/python3.6/bin/python3.6
+  elif [ -f /usr/bin/python3 ]; then
+      alias python=/usr/bin/python3
+  fi
+}
+
+setup_node
+setup_python
--- bin/on_pi_startup/startup_by_hostname/all.sh
+++ bin/on_pi_startup/startup_by_hostname/all.sh
@@ -2,13 +2,6 @@
 
 cd "$(dirname "$0")"
 
-# TODO: When we initially setup the raspberry pi, set the PATH (maybe in .profile) so we don't have to do this here
-node_folder=`(cd ~/.nvm/versions/node/**/bin/ && pwd)`
-echo $node_folder
-echo "Adding '$node_folder' to PATH"
-PATH="$node_folder:$PATH"
-node --version
-
 source ../../misc/setup_shell.sh
 
 echo "Clearing all stored process ids"
