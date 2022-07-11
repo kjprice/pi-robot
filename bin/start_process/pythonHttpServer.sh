f428e1563ce896ac43344f6c0be49180c922c73a
KJ Price <kjprice12@gmail.com>
2022-07-11 11:18:11 -0500

    Python http server should serve the data folder

--- bin/start_process/pythonHttpServer.sh
+++ bin/start_process/pythonHttpServer.sh
@@ -7,4 +7,5 @@ source ../misc/setup_shell.sh
 python_port=`../misc/get_config.sh portsByProcess.pythonHttpServer`
 ../misc/kill_process_on_port.sh $python_port
 
+cd ../../data/
 python -m http.server $python_port
