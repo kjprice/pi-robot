9c138f8c9d863de17d055ce0d8492c6e30b1ebfa
KJ Price <kjprice12@gmail.com>
2022-07-11 10:55:58 -0500

    Fix misspelling

--- bin/start_process/securityCamera.sh
+++ bin/start_process/securityCamera.sh
@@ -5,6 +5,6 @@ cd "$(dirname "$0")"
 source ../misc/setup_shell.sh
 
 # Kill services that might use the camera
-../misc/kill_process_by_name.sh robotCameraHea
+../misc/kill_process_by_name.sh robotCameraHead
 
 ../run/run_security_camera_forever.sh
