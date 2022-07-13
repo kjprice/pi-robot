442aa11261892c8d75448f60291bd99cb0423cc0
KJ Price <kjprice12@gmail.com>
2022-07-13 18:00:59 -0500

    Security camera - run python script

--- bin/start_process/securityCamera.sh
+++ bin/start_process/securityCamera.sh
@@ -2,9 +2,15 @@
 
 cd "$(dirname "$0")"
 
+other_args=$1
+
 source ../misc/setup_shell.sh
 
 # Kill services that might use the camera
 ../misc/kill_process_by_name.sh robotCameraHead
 
-../run/run_security_camera_forever.sh
+# ../run/run_security_camera_forever.sh
+
+cd ../../
+
+python -m python.pi_applications.run_security_camera $other_args
