68494d87df1071eb1b6101c9e3ccef711ddaeaf7
KJ Price <kjprice12@gmail.com>
2022-07-22 15:23:35 -0500

    Send output to file and stream

--- python/pi_applications/run_security_camera.py
+++ python/pi_applications/run_security_camera.py
@@ -7,6 +7,8 @@
 
 #!/usr/bin/python3
 
+print('just for testing -delete me')
+
 from multiprocessing import Pipe, Process
 from multiprocessing.connection import Connection
 
@@ -15,6 +17,12 @@ from ..modules.config import get_servo_url, SERVER_NAMES, get_port_by_name_from_
 from ..modules.server_module.server_module import ServerModule
 from ..modules.raspi_video.save_video_input import SaveVideoInput
 from ..modules.raspi_video.stream_video_input import StreamVideoInput
+from http.server import HTTPServer, SimpleHTTPRequestHandler, test
+import time
+
+from picamera2 import Picamera2
+from picamera2.encoders import H264Encoder
+from picamera2.outputs import FfmpegOutput, FileOutput
 
 class SecurityCameraOutput(ServerModule):
     from_video_pipe = None
@@ -82,10 +90,25 @@ class SecurityCamera(ServerModule):
 
         server_name = SERVER_NAMES.SECURITY_CAMERA
         self.other_thread_functions = (
-            self.init_output,
+            # self.init_output,
+            self.init_python_server,
         )
 
         super().__init__(server_name, arg_flags)
+    
+    def init_python_server(self):
+        print('starting python server')
+        class CORSRequestHandler (SimpleHTTPRequestHandler):
+            def end_headers (self):
+                self.send_header('Access-Control-Allow-Origin', '*')
+                SimpleHTTPRequestHandler.end_headers(self)
+
+        # TODO: Store in config
+        PYTHON_SERVER_PORT=8999
+        # TODO: Move to correct folder (/data/security_videos/)
+        test(CORSRequestHandler, HTTPServer, port=PYTHON_SERVER_PORT)
+        print('completed python server')
+
     
     def init_output(self):
         print('init_output')
@@ -103,12 +126,61 @@ class SecurityCamera(ServerModule):
             self.send_output('get_video')
             self.sleep(1)
 
+    # https://datasheets.raspberrypi.com/camera/picamera2-manual-draft.pdf
+    # Section 9.3 - multiple outputs
     def run_continuously(self):
+        picam2 = Picamera2()
+        video_config = picam2.video_configuration({"size": (640, 480)})
+        picam2.configure(video_config)
+
+        encoder = H264Encoder(bitrate=1000000, repeat=True, iperiod=15)
+        # encoder = H264Encoder(repeat=True, iperiod=15)
+        # output1 = FfmpegOutput("-f mpegts udp://<ip-address>:12345")
+        stream_output = FfmpegOutput("-f hls -hls_time 4 -hls_list_size 5 -hls_flags delete_segments -hls_allow_cache 0 data/security_videos/stream/stream.m3u8")
+        file_output = FileOutput()
+        encoder.output = [stream_output, file_output]
+
+        picam2.start_encoder(encoder)
+        picam2.start()
+
+
+        i = 0
         while True:
+            i = i + 1
             if self.abort_signal_received:
                 self.send_output('abort_signal_received - returning')
                 return
-            self.get_video()
+
+            # output = [
+                # FfmpegOutput("-f hls -hls_time 4 -hls_list_size 5 -hls_flags delete_segments -hls_allow_cache 0 data/security_videos/stream/stream.m3u8"),
+                # 'test_saved_video.h264'
+
+            # ]
+
+            start = time.time()
+
+            # picam2.start_recording(encoder, output)
+            file_path = "data/security_videos/stream/{}_test.h264".format(i)
+            self.send_output('Sending to {}'.format(file_path))
+            file_output.fileoutput = file_path
+            file_output.start()
+            self.sleep(5)
+            file_output.stop()
+
+            end = time.time()
+            time_diff = end - start
+            print(f'{time_diff} seconds passed')
+
+            if self.abort_signal_received:
+                # picam2.stop_recording()
+                picam2.stop()
+                return
+
+            # self.send_output('Pausing')
+            # self.sleep(15)
+            self.send_output('Rerunning')
+
+
 
 def start_camera_process(arg_flags=None):
     camera_head = SecurityCamera(arg_flags)
