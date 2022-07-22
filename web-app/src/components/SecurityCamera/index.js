826ec777de280c176dfb050067bbad409d163916
KJ Price <kjprice12@gmail.com>
2022-07-22 15:25:54 -0500

    Front end for streaming (WIP)

--- web-app/src/components/SecurityCamera/index.js
+++ web-app/src/components/SecurityCamera/index.js
@@ -21,16 +21,53 @@ const devicesRunningSecurityCamera = raspiStatusesByHostname => {
     return processes.includes(SECURITY_CAMERA_PROCESS_NAME);
   })
 }
+const STREAM_URL = 'http://pi3misc2:8999/data/security_videos/stream/stream.m3u8';
+
+let videoStarted = false;
+
+const startHls = () => {
+  console.log('starting video')
+
+  console.log(global)
+  if ( global.Hls.isSupported() ) {
+    console.log('settup hls')
+    var video = document.getElementById('video');
+    var hls = new global.Hls();
+    hls.loadSource(STREAM_URL);
+    hls.attachMedia(video);
+  }
+}
+
+const startVideo = () => {
+  if (videoStarted) {
+    console.log('video already started')
+    return;
+  }
+
+  videoStarted = true;
+
+  setTimeout(startHls, 1000);
+
+}
 
 function SecurityCamera(props) {
   const { raspiStatusesByHostname } = props;
 
   const hostnamesRunningSecurityCamera = devicesRunningSecurityCamera(raspiStatusesByHostname);
 
+  startVideo();
   return (
     <div>
       <h2>Devices Running Security Camera</h2>
       {hostnamesRunningSecurityCamera.map(hostname => <div key={hostname}>{hostname}</div>)}
+      <div>
+        Video Source: <a href={STREAM_URL}>{STREAM_URL}</a>
+      </div>
+      <video id="video" crossOrigin="use-credentials" autoPlay controls>
+        {/* <source src="http://pi3misc2:8999/stream.m3u8" type="application/x-mpegURL" /> */}
+        <source src={STREAM_URL} type="application/x-mpegURL" />
+        {/* <source src="http://pi3misc2:8000/stream.m3u8" type="application/x-mpegURL" /> */}
+      </video>
     </div>
   );
 }
