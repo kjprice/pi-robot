19301546afbec31571ee65679d3cb6298b2afcf0
KJ Price <kjprice12@gmail.com>
2022-07-24 11:11:14 -0500

    Remove unused files

--- bin/run/run_security_camera_forever.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")"
-
-while true
-do
-  ./run_security_camera.sh
-done
--- bin/run/run_test_security_camera.sh
+++
@@ -1,8 +0,0 @@
-cd "$(dirname "$0")"
-cd ../
-
-rm -r ../data/security_videos
-
-./misc/copy.sh
-ssh pi@pi3misc2 'bash ~/Projects/pirobot/bin/run/run_security_camera.sh'
-./download/download_security_videos.sh
