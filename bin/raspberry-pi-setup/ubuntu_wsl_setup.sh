09ffba5fb169b0a13b20d9a1a4bbafef7d70c3e0
KJ Price <kjprice12@gmail.com>
2022-07-25 15:30:57 -0500

    Install MongoDB Server for WSL

--- bin/raspberry-pi-setup/ubuntu_wsl_setup.sh
+++ bin/raspberry-pi-setup/ubuntu_wsl_setup.sh
@@ -25,3 +25,23 @@ python3 -m pip install --no-input psutil
 # TO start ssh on wsl ubuntu:
 # - sudo service ssh start
 # - See /README.md on how to automatically start ssh on wsl
+
+# MongoDB
+# https://docs.microsoft.com/en-us/windows/wsl/tutorials/wsl-database
+
+cd ~
+sudo apt update
+wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+mkdir -p ~/data/db
+
+# TO verify mongo is running
+# sudo mongod --dbpath ~/data/db
+# ps -e | grep 'mongod'
+
+# Have MongoDB start automatically
+curl https://raw.githubusercontent.com/mongodb/mongo/master/debian/init.d | sudo tee /etc/init.d/mongodb >/dev/null
+sudo chmod +x /etc/init.d/mongodb
+sudo service mongodb start 
