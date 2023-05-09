a70ad36849b57f03c791e90fb29e6c5d07c9df96
KJ Price <kjprice12@gmail.com>
2023-05-09 08:10:30 -0500

    Include all scrape-comics ports

--- bin/windows_scripts/forward_to_wsl_port/wsl_parameters.sh
+++ bin/windows_scripts/forward_to_wsl_port/wsl_parameters.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+source ./scrape_comics_ports.sh # SCRAPE_COMICS_PORTS
 
 WEB_SERVER_PORT=`../misc/get_config.sh ports.webServerPort`
 IMAGE_HUB_PORT=`../misc/get_config.sh ports.imageHubPort`
@@ -6,9 +7,11 @@ PYTHON_HTTP_SERVER_PORT=`../misc/get_config.sh portsByProcess.pythonHttpServer`
 NODE_SERVER_STATUS_PORT=`../misc/get_config.sh portsByProcess.nodeServerStatus`
 WSL_SSH_PORT=2929
 MONGO_PORT=27017
-COMIC_SCRAPE_PORT=9191
 
-PORTS=($WEB_SERVER_PORT $IMAGE_HUB_PORT $PYTHON_HTTP_SERVER_PORT $NODE_SERVER_STATUS_PORT $WSL_SSH_PORT $MONGO_PORT $COMIC_SCRAPE_PORT)
+PI_ROBOT_PORTS=($WEB_SERVER_PORT $IMAGE_HUB_PORT $PYTHON_HTTP_SERVER_PORT $NODE_SERVER_STATUS_PORT $WSL_SSH_PORT $MONGO_PORT)
+
+PORTS=(${PI_ROBOT_PORTS[@]})
+PORTS+=(${SCRAPE_COMICS_PORTS[@]})
 
 WSL_IP=`wsl hostname -I`
 WINDOWS_IP=`ipconfig|grep -m 1 IPv4|sed "s/IPv4 Address. . . . . . . . . . . : //g" | xargs`
