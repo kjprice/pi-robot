8c11e841af7250c45b9cf0164b28b70cba30e8bc
KJ Price <kjprice12@gmail.com>
2024-06-05 09:29:09 -0500

    Install helpful commands

--- bin/raspberry-pi-setup/common_dependencies.sh
+++ bin/raspberry-pi-setup/common_dependencies.sh
@@ -19,12 +19,7 @@ python3 -m pip pip install -U scikit-learn # sklearn
 # Concurrency
 python3 -m pip install --no-input eventlet
 
-
-# Read JSON in shell commands
-sudo apt-get install -y jq
-
-
-## Mongo
+# Mongo
 python3 -m pip install --no-input pymongo
 
 # Keyboard/mouse events
@@ -35,3 +30,34 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | 
 nvm install stable
 source  ~/.bashrc
 npm install -g nodemon
+
+
+##########################
+
+#### Helpful commands ####
+# Taken from https://www.infoworld.com/article/3715352/9-command-line-jewels-for-your-developer-toolkit.html
+
+# bat (like `cat` but better) https://github.com/sharkdp/bat?tab=readme-ov-file#installation
+sudo apt install bat
+mkdir -p ~/.local/bin
+ln -s /usr/bin/batcat ~/.local/bin/bat
+
+# fzf (fuzzy finder) https://github.com/junegunn/fzf?tab=readme-ov-file#installation
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+# Read JSON in shell commands
+sudo apt-get install -y jq 
+
+# tldr (like `man` but better)
+npm install -g tldr
+
+echo 'ngrok (awesome reverse proxy - make local servers public)'
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | \
+  sudo gpg --dearmor -o /etc/apt/keyrings/ngrok.gpg && \
+  echo "deb [signed-by=/etc/apt/keyrings/ngrok.gpg] https://ngrok-agent.s3.amazonaws.com buster main" | \
+  sudo tee /etc/apt/sources.list.d/ngrok.list && \
+  sudo apt update && sudo apt install ngrok
+ngrok config add-authtoken 1Qxzg0ySLAvaZFh6GkMBldXWFgz_2TV8GeWMgvWwy6eAJ6Hyg # Super secret token
+
+##########################
